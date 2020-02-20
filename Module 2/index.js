242c6e2505dafee00f4497db702b0a9bd7da4727
Rahul <neu_tron@yahoo.com>
2020-02-20 16:59:19 +0530

    Update index.js

--- Module 2/index.js
+++ Module 2/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const Datastore = require('nedb');
+
 const app = express();
-const locationArray = [];
+const database = new Datastore('database.db');
+database.loadDatabase();
 
 app.listen(3000, () => {
 	console.log('listening at 3000')
@@ -10,11 +13,17 @@ app.use(express.json({ limit: '1mb' }));
 
 // Routing
 app.post('/api', (request, response) => {
-	locationArray.push(request.body);
-	response.json(
-		locationArray
-	);
+	const data = request.body;
+	const timestamp = Date.now();
+	data.timestamp = timestamp;
+	database.insert(data);
+	response.json({
+		status: 'success',
+		latitude: data.lat,
+		longitude: data.long,
+		timestamp: data.timestamp,
+		image64: data.image64
+	});
 });
-
 
 
