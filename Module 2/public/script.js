5257ac25d7e0ba0b0e02c64072c2fdeb5eaa7c87
Rahul <neu_tron@yahoo.com>
2020-02-20 17:00:12 +0530

    Adding p5.js videoCapture for selfie

--- Module 2/public/script.js
+++ Module 2/public/script.js
@@ -1,7 +1,16 @@
-const button = document.getElementById('button');
-button.addEventListener('click', () => {
-	geolocationData();
-});
+let video;
+function setup() {
+
+	noCanvas();
+	video = createCapture(VIDEO);
+	video.size(320, 240);
+
+	const button = document.getElementById('button');
+	button.addEventListener('click', () => {
+		geolocationData();
+	});
+
+}
 
 function geolocationData() {
 	if ('geolocation' in navigator) {
@@ -11,17 +20,19 @@ function geolocationData() {
 			var long = position.coords.longitude.toFixed(2);
 			document.getElementById('latitude').textContent = lat
 			document.getElementById('longitude').textContent = long;
+			video.loadPixels();
+			var image64 = video.canvas.toDataURL();
 
-			var data = { lat, long };
-			
+			var data = { lat, long, image64 };
+
 			var options = {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json'
 				},
-				body: JSON.stringify(data) 
+				body: JSON.stringify(data)
 			};
-			
+
 			fetch('/api', options)
 				.then(response => {
 					return response.json();
@@ -29,9 +40,9 @@ function geolocationData() {
 				.then(data => {
 					console.log(data);
 				})
-			
+
 		});
-	
+
 	} else {
 		console.log('No Geolocation');
 	}
